fb1ab74b55e0a492576b573430f9586d390c530b
James Alhassan <jamesalhassan052@gmail.com>
2023-04-25 03:58:18 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''GET request for employee's  TODO list progress'''
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    empUrl = baseUrl + "/" + empId
+
+    response = requests.get(empUrl)
+    employeeName = response.json()['name']
+
+    todoUrl = empUrl + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    done = 0
+    compTasks = []
+
+    for task in tasks:
+        if task['completed']:
+            compTasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in compTasks:
+        print("\t {}".format(task['title']))
