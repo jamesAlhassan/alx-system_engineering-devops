3aac19beca0a9613d9a6da105e2b5156fd8f4d65
James Alhassan <jamesalhassan052@gmail.com>
2023-04-25 04:11:02 +0000

    Add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''GET request for employee's  TODO list progress
+export results to csv'''
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    empUrl = baseUrl + "/" + empId
+
+    response = requests.get(empUrl)
+    userName = response.json()['username']
+
+    todoUrl = empUrl + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(empId), 'w') as f:
+        for task in tasks:
+            f.write('"{}","{}","{}","{}"\n'
+                       .format(empId, userName, task['completed'],
+                               task['title']))
