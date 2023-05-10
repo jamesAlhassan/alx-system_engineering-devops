a2cbe4563965329060e22be76b1114ace756c5bd
James Alhassan <jamesalhassan052@gmail.com>
2023-05-10 03:52:11 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = get(url, headers=user_agent)
+    response = requests.get(url, headers=user_agent)
     results = response.json()
 
     try:
