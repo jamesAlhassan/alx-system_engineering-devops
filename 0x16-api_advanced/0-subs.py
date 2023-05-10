ea75bea722c33933defd6ed916334e7ebc8e7206
James Alhassan <jamesalhassan052@gmail.com>
2023-05-10 03:56:53 +0000

    Update 0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Number of subscribers  (not active users, total subscribers) for a given subreddit
+Number of subscribers(not active users,total subscribers) for a given subreddit
 """
 
 import requests
